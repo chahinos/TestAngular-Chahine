6d79ec5d1b62ca4cd90204e90316ab7bea777860
chahinos <chahine.zaghdoudi@esprit.tn>
2026-03-05 10:55:48 +0100

    votre message de mise à jour

--- src/app/services/Atelier.service.ts
+++ src/app/services/Atelier.service.ts
@@ -7,21 +7,26 @@ import { Atelier } from '../models/Atelier';
   providedIn: 'root'
 })
 export class AtelierService {
-  private apiUrl = 'http://localhost:3000/Atelier'; // URL mte3 el backend (json-server par ex)
-
+  private apiUrl = 'http://localhost:3000/ateliers'; 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Atelier[]> { return this.http.get<Atelier[]>(this.apiUrl); }
+  getAll(): Observable<Atelier[]> { 
+    return this.http.get<Atelier[]>(this.apiUrl); 
+  }
   
-  getById(id: string): Observable<Atelier> { return this.http.get<Atelier>(`${this.apiUrl}/${id}`); }
+  getById(id: string): Observable<Atelier> { 
+    return this.http.get<Atelier>(`${this.apiUrl}/${id}`); 
+  }
 
-  add(Atelier: Atelier): Observable<Atelier> { return this.http.post<Atelier>(this.apiUrl, Atelier); }
+  add(atelier: Atelier): Observable<Atelier> { 
+    return this.http.post<Atelier>(this.apiUrl, atelier); 
+  }
 
-  update(id: string, Atelier: Atelier): Observable<Atelier> { return this.http.put<Atelier>(`${this.apiUrl}/${id}`, Atelier); }
+  update(id: string, atelier: Atelier): Observable<Atelier> { 
+    return this.http.put<Atelier>(`${this.apiUrl}/${id}`, atelier); 
+  }
 
-  delete(id: string): Observable<any> { return this.http.delete(`${this.apiUrl}/${id}`); }
-
-
- 
-  
+  delete(id: string): Observable<any> { 
+    return this.http.delete(`${this.apiUrl}/${id}`); 
+  }
 }
